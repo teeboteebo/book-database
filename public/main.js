9c63e2e27ce391acc9a6b6c61baafd4577097cd5
Jesper Asplund <jesper.asplund95@gmail.com>
2019-01-16 12:24:43 +0100

    Added: Add book-feature and "missing-jpg"

--- public/main.js
+++ public/main.js
@@ -1,25 +1,23 @@
 async function start() {
   //      Get all books
-  // const books = await getBooks();
-  // console.log(books);
+  const books = await getBooks();
+  console.log(books);
 
-  //      Create new book
-  // let newBook = {
-  //   "author": "Dennis2",
-  //   "country": "Sweden2",
-  //   "image": "missing.jpg",
-  //   "language": "Swedish",
-  //   "link": "no-link2",
-  //   "pages": 1234,
-  //   "title": "No title2",
-  //   "year": 2018
-  // };
+  //  Create new book
+  let newBook = {
+    "author": $('#author-query').text(),
+    "country": $('#country-query').text(),
+    "image": "missing.jpg",
+    "language": $('#language-query').text(),
+    "link": "www.google.com",
+    "pages": $('#pages-query').val(),
+    "title": $('#title-query').text(),
+    "year": $('#year-query').val()
+  };
 
   // newBook = await createNewBook(newBook);
-  // console.log('newBook: ', newBook);
+  console.log('newBook: ', newBook);
 }
-
-start();
 
 // LOAD ALL BOOKS
 $('#get-books').click(async () => {
@@ -140,6 +138,7 @@ $('#search-country').click(async () => {
         <p><strong>More information:</strong></p>
         <p><a href="${book.link}">Wikipedia</a></p>
 
+        <button type="button" class="btn btn-primary mt-3 edit" data-id="${book._id}">Edit</button>
         <button type="button" class="btn btn-danger mt-3 delete" data-id="${book._id}">Delete</button>
       </div>
       `
@@ -316,14 +315,158 @@ $('#search-year').click(async () => {
   };
 });
 
-/* $('#delete').click(() => {
-  let bookId = $(this).attr('data-id')
-  deleteBook(bookId);
-}); */
-
 $('.parent').on('click', '.delete', (e) => {
   let bookId = $(e.target).attr('data-id')
   deleteBook(bookId);
   $(e.target).parent().remove();
 
 });
+
+$('#add-book').click(async () => {
+  $('.filter-container').empty();
+  $('.filter-container').append(
+    `
+      <div class="col-6 text-center">
+        <p>Author</p>
+        <div class="input-group mb-3">
+          <input id="author-query" type="text" class="form-control" placeholder="Author" aria-label="Recipient's username"
+            aria-describedby="button-addon2">
+        </div>
+        <p>Country</p>
+        <div class="input-group mb-3">
+          <input id="country-query" type="text" class="form-control" placeholder="Country" aria-label="Recipient's username"
+            aria-describedby="button-addon2">
+        </div>
+        <p>Language</p>
+        <div class="input-group mb-3">
+          <input id="language-query" type="text" class="form-control" placeholder="Language" aria-label="Recipient's username"
+            aria-describedby="button-addon2">
+        </div>
+      </div>
+      <div class="col-6 text-center">
+        <p>Pages</p>
+        <div class="input-group mb-3">
+          <input id="pages-query" type="number" class="form-control" placeholder="Number of pages" aria-label="Recipient's username"
+            aria-describedby="button-addon2">
+        </div>
+        <p>Title</p>
+        <div class="input-group mb-3">
+          <input id="title-query" type="text" class="form-control" placeholder="Title" aria-label="Recipient's username"
+            aria-describedby="button-addon2">
+        </div>
+        <p>Year</p>
+        <div class="input-group mb-3">
+          <input id="year-query" type="number" class="form-control" placeholder="Year" aria-label="Recipient's username"
+            aria-describedby="button-addon2">
+        </div>
+      </div>
+
+      <div class="col-12 text-center mt-3">
+        <button id="submit-book" type="button" class="btn btn-success">Submit book</button>
+      </div>
+    `
+
+  );
+});
+
+
+
+
+$('.parent').on('click', '#submit-book', (async () => {
+  let newBook = {
+    "author": $('#author-query').val(),
+    "country": $('#country-query').val(),
+    "image": "missing.jpg",
+    "language": $('#language-query').val(),
+    "link": "https://en.wikipedia.org/wiki/Book",
+    "pages": $('#pages-query').val()*1,
+    "title": $('#title-query').val(),
+    "year": $('#year-query').val()*1
+  };
+
+  newBook = await createNewBook(newBook);
+  console.log('newBook: ', newBook);
+  
+  $('.filter-container').empty();
+
+  $('.filter-container').append(`
+    <div class="col-6 text-center">
+      <p>Author</p>
+      <div class="input-group mb-3">
+        <input id="author-query" type="text" class="form-control" placeholder="Author" aria-label="Recipient's username"
+          aria-describedby="button-addon2">
+        <div class="input-group-append">
+          <button id="search-author" class="btn btn-outline-secondary" type="button">Search</button>
+        </div>
+      </div>
+      <p>Country</p>
+      <div class="input-group mb-3">
+        <input id="country-query" type="text" class="form-control" placeholder="Country" aria-label="Recipient's username"
+          aria-describedby="button-addon2">
+        <div class="input-group-append">
+          <button id="search-country" class="btn btn-outline-secondary" type="button">Search</button>
+        </div>
+      </div>
+      <p>Language</p>
+      <div class="input-group mb-3">
+        <input id="language-query" type="text" class="form-control" placeholder="Language" aria-label="Recipient's username"
+          aria-describedby="button-addon2">
+        <div class="input-group-append">
+          <button id="search-language" class="btn btn-outline-secondary" type="button">Search</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-6 text-center">
+      <!--Pages-->
+      <div class="row">
+        <div class="col-12">
+          <p>Pages</p>
+        </div>
+        <div class="col-6">
+          <div class="input-group mb-3">
+            <input id="minPages-query" type="text" class="form-control" placeholder="From" aria-label="Recipient's username"
+              aria-describedby="button-addon2">
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="input-group mb-3">
+            <input id="maxPages-query" type="text" class="form-control" placeholder="To" aria-label="Recipient's username"
+              aria-describedby="button-addon2">
+            <div class="input-group-append">
+              <button id="search-pages" class="btn btn-outline-secondary" type="button">Search</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p>Title</p>
+      <div class="input-group mb-3">
+        <input id="title-query" type="text" class="form-control" placeholder="Title" aria-label="Recipient's username"
+          aria-describedby="button-addon2">
+        <div class="input-group-append">
+          <button id="search-title" class="btn btn-outline-secondary" type="button">Search</button>
+        </div>
+      </div>
+      <!--Year-->
+      <div class="row">
+        <div class="col-12">
+          <p>Year</p>
+        </div>
+        <div class="col-6">
+          <div class="input-group mb-3">
+            <input id="year1-query" type="text" class="form-control" placeholder="From" aria-label="Recipient's username"
+              aria-describedby="button-addon2">
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="input-group mb-3">
+            <input id="year2-query" type="text" class="form-control" placeholder="To" aria-label="Recipient's username"
+              aria-describedby="button-addon2">
+            <div class="input-group-append">
+              <button id="search-year" class="btn btn-outline-secondary" type="button">Search</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `);
+}));
